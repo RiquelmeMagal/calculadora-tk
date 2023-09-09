546a3a11a096afb2a7a7ae0b5e874441915afb6e
João Victor Vilela dos Santos <118611278+oaojcc@users.noreply.github.com>
2023-09-09 16:21:04 -0300

    Style: removendo comentários desnecessários

--- Calculadora TK/CalculadoraTK.py
+++ Calculadora TK/CalculadoraTK.py
@@ -28,18 +28,14 @@ def formatar_numero(numero):
     """
     Formata um número para exibição, arredondando para até 10 casas decimais, se necessário.
     """
-    return round(numero, 10) if len(str(numero))  > 10 else numero
+    return round(numero, 7) if len(str(numero))  > 7 else numero
         
 def obter_caractere(e):
     """
     Atualiza o acumulador e a expressão atual com um caractere fornecido.
     """
     global acumulador, expressao_atual, ultimo_caractere
-
-    # Adiciona o caractere recebido ao acumulador
     acumulador += str(e)
-
-    # Atualiza a expressão atual com o valor do acumulador
     expressao_atual.set(acumulador)
 
 def limpar_display():
@@ -47,10 +43,7 @@ def limpar_display():
     Limpa o display da calculadora, redefinindo o acumulador e a expressão atual.
     """
     global acumulador, expressao_atual
-
-    # Redefine o acumulador para uma string vazia
     acumulador = ''
-    # Atualiza a expressão atual para uma string vazia
     expressao_atual.set('')
 
 def tratar_expressao(expressao):
@@ -119,21 +112,16 @@ def calcular_expressao():
 
     if len(acumulador) > 0:
         try:
-            # Avalia a expressão no acumulador
             resultado = eval(tratar_expressao(acumulador))
 
-            # Verifica se o resultado é um número inteiro 
             if int(resultado) == resultado:
                 expressao_atual.set(int(resultado))
             else:   
-                # Formata o resultado e atualiza a expressão atual
                 resultado_formatado = formatar_numero(resultado)
                 expressao_atual.set(resultado_formatado)
 
-            # Atualiza o acumulador com a expressão atual
             acumulador = expressao_atual.get()
         except:
-            # Em caso de erro na expressão, mostra uma mensagem de erro
             showerror('Erro', 'Expressão mal formada')
             limpar_display()
 
@@ -242,7 +230,45 @@ def janela_sobre():
     label_repository.bind("<Button-1>", lambda e: callback('https://github.com/oaojcc/calculadora-tk'))
     label_repository.place(x=0, y=175)
 
-# Cria um dicionário que mapeia os dígitos para suas posições na grade.
+janela = Tk()
+janela.title('Calculadora TK')
+centralizar_janela(janela, 388, 574)
+janela.resizable(0, 0)
+janela.wm_attributes('-topmost', True)
+
+expressao_atual = StringVar()
+texto_botao_rad_deg = StringVar(value='RAD')
+
+menubar = Menu(janela)
+menu_opt = Menu(menubar, tearoff=FALSE)
+sub_menu = Menu(menu_opt, tearoff=0)
+sub_menu.add_command(label='Padrão', command=lambda:janela_modo_default()) 
+sub_menu.add_command(label='Científica', command=lambda:janela_modo_cientifico()) 
+
+menu_opt.add_cascade(
+    label='Calculadora',
+    menu=sub_menu
+)
+
+menu_opt.add_separator()
+menu_opt.add_command(label='Sair', command=lambda:sair())
+menubar.add_cascade(label='Opções', menu=menu_opt)
+menu_sobre = Menu(menubar, tearoff=FALSE)
+menu_sobre.add_command(label='Sobre a Calculadora TK', command=lambda:janela_sobre())
+menubar.add_cascade(label='Ajuda', menu=menu_sobre)
+
+janela.config(menu=menubar)
+
+frame_display = Frame(janela, height=15, background='#f5f5f5')
+frame_display.pack(fill=BOTH)
+
+texto_display = Entry(frame_display, textvariable=expressao_atual, background='#f5f5f5', font=('Arial', 50, 'bold'), state=DISABLED, borderwidth=9, justify=RIGHT)
+texto_display.pack(side=RIGHT)
+
+frame_botao = Frame(janela, background='black')
+
+frame_botao.pack(fill=BOTH)
+
 caracteres = {
         '(': (0, 2), ')': (0, 3), '/': (0, 4),
         7: (1, 1), 8: (1, 2), 9: (1, 3), '*': (1, 4),
@@ -251,90 +277,14 @@ caracteres = {
         0: (4, 2), '.': (4, 1),
 }
 
-# Cria uma janela do Tkinter.
-janela = Tk()
-
-# Define o título da janela como "Calculadora TK".
-janela.title('Calculadora TK')
-
-# Define o tamanho da janela para 388 pixels de largura e 574 pixels de altura.
-centralizar_janela(janela, 388, 574)
-
-# Define que a janela não poderá ser redimensionada pelo usuário
-janela.resizable(0, 0)
-
-# sobreposição de app
-janela.wm_attributes('-topmost', True)
-
-# StringVar
-expressao_atual = StringVar()
-texto_botao_rad_deg = StringVar(value='RAD')
-
-# Menu
-# Criação do objeto Menu e associação à janela
-menubar = Menu(janela)
-
-# Criação de um submenu com a opção de não destacar o primeiro item
-menu_opt = Menu(menubar, tearoff=FALSE)
-
-# Criação de um submenu aninhado
-sub_menu = Menu(menu_opt, tearoff=0)
-sub_menu.add_command(label='Padrão', command=lambda:janela_modo_default())  # Adiciona a opção 'Padrão' ao submenu
-sub_menu.add_command(label='Científica', command=lambda:janela_modo_cientifico())  # Adiciona a opção 'Científica' ao submenu
-
-# Associa o submenu aninhado ao item 'Calculadora' do menu principal
-menu_opt.add_cascade(
-    label='Calculadora',
-    menu=sub_menu
-)
-
-# Adiciona uma separação visual no menu principal
-menu_opt.add_separator()
-
-# Adiciona a opção 'Sair' ao menu principal, com a ação de fechar a janela quando selecionada
-menu_opt.add_command(label='Sair', command=lambda:sair())
-
-# Adiciona o menu principal ('Calculadora' e 'Sair') ao menu principal da janela
-menubar.add_cascade(label='Opções', menu=menu_opt)
-
-# Criação de um submenu separado ('Ajuda')
-menu_sobre = Menu(menubar, tearoff=FALSE)
-
-# Adiciona a opção 'Sobre' ao submenu 'Ajuda', com ação para exibir informações sobre a aplicação
-menu_sobre.add_command(label='Sobre a Calculadora TK', command=lambda:janela_sobre())
-
-# Adiciona o submenu 'Ajuda' ao menu principal da janela
-menubar.add_cascade(label='Ajuda', menu=menu_sobre)
-
-# Configura o menu criado como o menu da janela
-janela.config(menu=menubar)
-
-# Cria um contêiner para o display.
-frame_display = Frame(janela, height=15, background='#f5f5f5')
-
-# Adiciona o contêiner para o display à janela.
-frame_display.pack(fill=BOTH)
-
-texto_display = Entry(frame_display, textvariable=expressao_atual, background='#f5f5f5', font=('Arial', 50, 'bold'), state=DISABLED, borderwidth=9, justify=RIGHT)
-texto_display.pack(side=RIGHT)
-
-# Cria um contêiner para os botões.
-frame_botao = Frame(janela, background='black')
-
-# Adiciona o contêiner para os botões à janela.
-frame_botao.pack(fill=BOTH)
-
-# Cria um botão para cada dígito e o adiciona ao contêiner para os botões.
 for caractere, posicao in caracteres.items():
     botao = criar_botao(str(caractere), lambda x=str(caractere): obter_caractere(x))
     if caractere in [0,1,2,3,4,5,6,7,8,9,'.']:
             botao.configure(font=('Arial', 24, 'bold'))
     botao.grid(row=posicao[0], column=posicao[1], sticky=NSEW)
 
-# boão apagar
 botao_apagar = criar_botao('C', command=lambda:limpar_display()).grid(row=0, column=1, sticky=NSEW)
 
-# boão igual
 botao_igual = criar_botao('=', command=lambda:calcular_expressao())
 botao_igual.configure(background='#daffcc', 
                       foreground='green', 
@@ -342,5 +292,4 @@ botao_igual.configure(background='#daffcc',
                       activeforeground='green')
 botao_igual.grid(row=4, column=3, sticky=NSEW, columnspan=2)
 
-# Inicia o loop principal do Tkinter, que faz com que a janela seja exibida e o programa permaneça em execução até que o usuário feche a janela.
 janela.mainloop()
